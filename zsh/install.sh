8ff2772f4173b39d8c420516ae24bc4780389b34
Lukas Vogel <mail@lukas-vogel.ch>
2023-12-05 09:10:54 +0100

    Add `powerlevel10k/powerlevel10k` as ZSH theme

---
+++ zsh/install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install powerlevel10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
